15635ef1e0e3da37b8d1ee3744aa79b453066050
hzindia <hz.india@gmail.com>
2026-03-20 23:24:03 +0530

    gradient clipping (max_norm=1.0)

--- autoresearch/train_vae.py
+++ autoresearch/train_vae.py
@@ -125,6 +125,7 @@ def train():
             recon, mu, logvar = model(x)
             loss = vae_loss(recon, x, mu, logvar)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             epoch_loss += loss.item()
             batches += 1
